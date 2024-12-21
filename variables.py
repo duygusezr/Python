afc06dbcfa921eb4ff4bc3dbe2412736efec426b
Duygu <148623218+duygusezr@users.noreply.github.com>
2024-12-21 13:14:37 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,48 @@
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+#Integers
+age = 21
+quantity = 3
+num_of_students = 30
+
+#Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+#Boolen
+
+is_student = True 
+for_sale = True
+is_online = False 
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT available")
+
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are Not a student")
+
+if is_online:
+    print("That item is online")
+else:
+    print("That item is NOT online")
+    
+
+print(f"Are you a student: {is_student}")
+
+print (f"The price is ${price}")
+print (f"Your gpa is: {gpa}")
+print (f"You ran {distance}km")
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your mail: {email}")

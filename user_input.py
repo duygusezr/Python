0085a7db8f64e81229aa07fee6daddc229262c76
Duygu <148623218+duygusezr@users.noreply.github.com>
2024-12-21 14:22:39 +0300

    input

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = input ("How old are you?: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You are {age} years old")
